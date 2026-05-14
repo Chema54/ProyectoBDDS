60d1b12ab498bc65d403567c78c5a9fd3f0b9bc7
LeninRevan <lenintanooki@gmail.com>
2026-05-14 12:09:08 -0600

    Rename de el paquete aplication

---
+++ src/main/application/FXMLInicioSesionController.java
@@ -0,0 +1,46 @@
+package main.application;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author leninrevan
+ */
+public class FXMLInicioSesionController implements Initializable {
+
+    @FXML
+    private TextField tf_usuario;
+    @FXML
+    private PasswordField tf_password;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void btn_IniciarSesion(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void btn_Salir(ActionEvent event) {
+        System.exit(0);
+    }
+    
+}
--- src/main/application/FXMLMenuCentralController.java
+++ src/main/application/FXMLMenuCentralController.java
@@ -1,29 +1,19 @@
-package main.aplication;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
+package main.application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
  *
  * @author leninrevan
  */
-public class FXMLInicioSesionController implements Initializable {
-
-    @FXML
-    private TextField tf_usuario;
-    @FXML
-    private PasswordField tf_password;
+public class FXMLMenuCentralController implements Initializable {
 
     /**
      * Initializes the controller class.
@@ -32,15 +22,5 @@ public class FXMLInicioSesionController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-
-    @FXML
-    private void btn_IniciarSesion(ActionEvent event) {
-        
-    }
-
-    @FXML
-    private void btn_Salir(ActionEvent event) {
-        System.exit(0);
-    }
     
 }
