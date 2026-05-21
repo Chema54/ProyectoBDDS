a7a1ddad3bd09b93d899ccaa727268914f6f4125
Chema54 <josemariacm005@gmail.com>
2026-05-21 09:08:01 -0600

    git- commit: Add Inventario DAO && DTO

---
+++ src/main/business/dao/InventarioDAO.java
@@ -0,0 +1,187 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.business.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import main.business.dao.shape.CompleteDAOShape;
+import main.business.dto.InventarioDTO;
+import main.common.ExceptionHandler;
+import main.common.UserDisplayableException;
+import main.database.DBConnector;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author josem
+ */
+public class InventarioDAO extends CompleteDAOShape<InventarioDTO, Integer> {
+
+    private static final Logger LOGGER = LogManager.getLogger(InventarioDAO.class);
+
+    private static final String CREATE_QUERY =
+            "INSERT INTO Inventario (id_sucursal, id_articulo, cantidad, stock_minimo, stock_maximo) VALUES (?, ?, ?, ?, ?)";
+
+    private static final String GET_ALL_QUERY =
+            "SELECT * FROM Inventario";
+
+    private static final String GET_QUERY =
+            "SELECT * FROM Inventario WHERE id_articulo = ?";
+
+    private static final String UPDATE_QUERY =
+            "UPDATE Inventario SET cantidad = ?, stock_minimo = ?, stock_maximo = ? WHERE id_sucursal = ? AND id_articulo = ?";
+
+    private static final String DELETE_QUERY =
+            "DELETE FROM Inventario WHERE id_articulo = ?";
+
+    @Override
+    public InventarioDTO getDTOInstanceFromResultSet(ResultSet resultSet) throws SQLException {
+
+        return new InventarioDTO.InventarioBuilder()
+                .setIDSucursal(resultSet.getInt("id_sucursal"))
+                .setIDArticulo(resultSet.getInt("id_articulo"))
+                .setCantidad(resultSet.getString("cantidad"))
+                .setStockMinimo(resultSet.getString("stock_minimo"))
+                .setStockMaximo(resultSet.getString("stock_maximo"))
+                .build();
+    }
+
+    @Override
+    public void createOne(InventarioDTO inventarioDTO) throws UserDisplayableException {
+
+        try (
+                Connection connection = DBConnector.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement(CREATE_QUERY)
+        ) {
+
+            statement.setInt(1, inventarioDTO.getIDSucursal());
+            statement.setInt(2, inventarioDTO.getIDArticulo());
+            statement.setString(3, inventarioDTO.getCantidad());
+            statement.setString(4, inventarioDTO.getStockMinimo());
+            statement.setString(5, inventarioDTO.getStockMaximo());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            throw ExceptionHandler.handleSQLException(
+                    LOGGER,
+                    e,
+                    "No ha sido posible crear el inventario."
+            );
+        }
+    }
+
+    @Override
+    public List<InventarioDTO> getAll() throws UserDisplayableException {
+
+        try (
+                Connection connection = DBConnector.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement(GET_ALL_QUERY);
+                ResultSet resultSet = statement.executeQuery()
+        ) {
+
+            List<InventarioDTO> list = new ArrayList<>();
+
+            while (resultSet.next()) {
+                list.add(getDTOInstanceFromResultSet(resultSet));
+            }
+
+            return list;
+
+        } catch (SQLException e) {
+
+            throw ExceptionHandler.handleSQLException(
+                    LOGGER,
+                    e,
+                    "No ha sido posible cargar el inventario."
+            );
+        }
+    }
+
+    @Override
+    public InventarioDTO getOne(Integer id) throws UserDisplayableException {
+
+        try (
+                Connection connection = DBConnector.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement(GET_QUERY)
+        ) {
+
+            statement.setInt(1, id);
+
+            InventarioDTO inventarioDTO = null;
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+
+                if (resultSet.next()) {
+                    inventarioDTO = getDTOInstanceFromResultSet(resultSet);
+                }
+            }
+
+            return inventarioDTO;
+
+        } catch (SQLException e) {
+
+            throw ExceptionHandler.handleSQLException(
+                    LOGGER,
+                    e,
+                    "No ha sido posible obtener el inventario."
+            );
+        }
+    }
+
+    @Override
+    public void updateOne(InventarioDTO inventarioDTO) throws UserDisplayableException {
+
+        try (
+                Connection connection = DBConnector.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY)
+        ) {
+
+            statement.setString(1, inventarioDTO.getCantidad());
+            statement.setString(2, inventarioDTO.getStockMinimo());
+            statement.setString(3, inventarioDTO.getStockMaximo());
+            statement.setInt(4, inventarioDTO.getIDSucursal());
+            statement.setInt(5, inventarioDTO.getIDArticulo());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            throw ExceptionHandler.handleSQLException(
+                    LOGGER,
+                    e,
+                    "No ha sido posible actualizar el inventario."
+            );
+        }
+    }
+
+    @Override
+    public void deleteOne(Integer id) throws UserDisplayableException {
+
+        try (
+                Connection connection = DBConnector.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement(DELETE_QUERY)
+        ) {
+
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            throw ExceptionHandler.handleSQLException(
+                    LOGGER,
+                    e,
+                    "No ha sido posible eliminar el inventario."
+            );
+        }
+    }
+}
---
+++ src/main/business/dto/InventarioDTO.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.business.dto;
+
+/**
+ *
+ * @author josem
+ */
+public class InventarioDTO {
+
+    private final int idSucursal;
+    private final int idArticulo;
+    private final String cantidad;
+    private final String stockMinimo;
+    private final String stockMaximo;
+
+    public InventarioDTO(InventarioBuilder builder) {
+        this.idSucursal = builder.idSucursal;
+        this.idArticulo = builder.idArticulo;
+        this.cantidad = builder.cantidad;
+        this.stockMinimo = builder.stockMinimo;
+        this.stockMaximo = builder.stockMaximo;
+    }
+
+    public int getIDSucursal() {
+        return idSucursal;
+    }
+
+    public int getIDArticulo() {
+        return idArticulo;
+    }
+
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public String getStockMinimo() {
+        return stockMinimo;
+    }
+
+    public String getStockMaximo() {
+        return stockMaximo;
+    }
+
+    @Override
+    public boolean equals(Object instance) {
+        if (this == instance) return true;
+        if (instance == null || getClass() != instance.getClass()) return false;
+
+        InventarioDTO that = (InventarioDTO) instance;
+
+        return idSucursal == that.idSucursal
+                && idArticulo == that.idArticulo
+                && cantidad.equals(that.cantidad)
+                && stockMinimo.equals(that.stockMinimo)
+                && stockMaximo.equals(that.stockMaximo);
+    }
+
+    public static class InventarioBuilder {
+
+        protected int idSucursal;
+        protected int idArticulo;
+        protected String cantidad;
+        protected String stockMinimo;
+        protected String stockMaximo;
+
+        public InventarioBuilder setIDSucursal(int idSucursal) {
+            this.idSucursal = idSucursal;
+            return this;
+        }
+
+        public InventarioBuilder setIDArticulo(int idArticulo) {
+            this.idArticulo = idArticulo;
+            return this;
+        }
+
+        public InventarioBuilder setCantidad(String cantidad) {
+            this.cantidad = cantidad;
+            return this;
+        }
+
+        public InventarioBuilder setStockMinimo(String stockMinimo) {
+            this.stockMinimo = stockMinimo;
+            return this;
+        }
+
+        public InventarioBuilder setStockMaximo(String stockMaximo) {
+            this.stockMaximo = stockMaximo;
+            return this;
+        }
+
+        public InventarioDTO build() {
+            return new InventarioDTO(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Sucursal: " + idSucursal
+                + ", Articulo: " + idArticulo
+                + ", Cantidad: " + cantidad
+                + ", Stock mínimo: " + stockMinimo
+                + ", Stock máximo: " + stockMaximo;
+    }
+}
