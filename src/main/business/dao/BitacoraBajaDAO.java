c20a3e1be67d8ed900cfef406a4f8a4f21603512
Chema54 <josemariacm005@gmail.com>
2026-05-21 12:37:01 -0600

    git- commit: Add BitacoraBaja DAO && DTO

---
+++ src/main/business/dao/BitacoraBajaDAO.java
@@ -0,0 +1,188 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.business.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import main.business.dao.shape.CompleteDAOShape;
+import main.business.dto.BitacoraBajaDTO;
+import main.common.ExceptionHandler;
+import main.common.UserDisplayableException;
+import main.database.DBConnector;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author josem
+ */
+public class BitacoraBajaDAO extends CompleteDAOShape<BitacoraBajaDTO, Integer> {
+
+    private static final Logger LOGGER = LogManager.getLogger(BitacoraBajaDAO.class);
+
+    private static final String CREATE_QUERY =
+            "INSERT INTO BitacoraBaja (id_bitacora, id_articulo, fecha, motivo, cantidad_restante) VALUES (?, ?, ?, ?, ?)";
+
+    private static final String GET_ALL_QUERY =
+            "SELECT * FROM BitacoraBaja";
+
+    private static final String GET_QUERY =
+            "SELECT * FROM BitacoraBaja WHERE id_bitacora = ?";
+
+    private static final String UPDATE_QUERY =
+            "UPDATE BitacoraBaja SET id_articulo = ?, fecha = ?, motivo = ?, cantidad_restante = ? WHERE id_bitacora = ?";
+
+    private static final String DELETE_QUERY =
+            "DELETE FROM BitacoraBaja WHERE id_bitacora = ?";
+
+    @Override
+    public BitacoraBajaDTO getDTOInstanceFromResultSet(ResultSet resultSet) throws SQLException {
+
+        return new BitacoraBajaDTO.BitacoraBajaBuilder()
+                .setIDBitacora(resultSet.getInt("id_bitacora"))
+                .setIDArticulo(resultSet.getInt("id_articulo"))
+                .setFecha(resultSet.getDate("fecha"))
+                .setMotivo(resultSet.getString("motivo"))
+                .setCantidadRestante(resultSet.getInt("cantidad_restante"))
+                .build();
+    }
+
+    @Override
+    public void createOne(BitacoraBajaDTO bitacoraBajaDTO) throws UserDisplayableException {
+
+        try (
+                Connection connection = DBConnector.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement(CREATE_QUERY)
+        ) {
+
+            statement.setInt(1, bitacoraBajaDTO.getIDBitacora());
+            statement.setInt(2, bitacoraBajaDTO.getIDArticulo());
+            statement.setDate(3, new Date(bitacoraBajaDTO.getFecha().getTime()));
+            statement.setString(4, bitacoraBajaDTO.getMotivo());
+            statement.setInt(5, bitacoraBajaDTO.getCantidadRestante());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            throw ExceptionHandler.handleSQLException(
+                    LOGGER,
+                    e,
+                    "No ha sido posible crear la bitácora de baja."
+            );
+        }
+    }
+
+    @Override
+    public List<BitacoraBajaDTO> getAll() throws UserDisplayableException {
+
+        try (
+                Connection connection = DBConnector.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement(GET_ALL_QUERY);
+                ResultSet resultSet = statement.executeQuery()
+        ) {
+
+            List<BitacoraBajaDTO> list = new ArrayList<>();
+
+            while (resultSet.next()) {
+                list.add(getDTOInstanceFromResultSet(resultSet));
+            }
+
+            return list;
+
+        } catch (SQLException e) {
+
+            throw ExceptionHandler.handleSQLException(
+                    LOGGER,
+                    e,
+                    "No ha sido posible cargar las bitácoras de baja."
+            );
+        }
+    }
+
+    @Override
+    public BitacoraBajaDTO getOne(Integer id) throws UserDisplayableException {
+
+        try (
+                Connection connection = DBConnector.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement(GET_QUERY)
+        ) {
+
+            statement.setInt(1, id);
+
+            BitacoraBajaDTO bitacoraBajaDTO = null;
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+
+                if (resultSet.next()) {
+                    bitacoraBajaDTO = getDTOInstanceFromResultSet(resultSet);
+                }
+            }
+
+            return bitacoraBajaDTO;
+
+        } catch (SQLException e) {
+
+            throw ExceptionHandler.handleSQLException(
+                    LOGGER,
+                    e,
+                    "No ha sido posible obtener la bitácora de baja."
+            );
+        }
+    }
+
+    @Override
+    public void updateOne(BitacoraBajaDTO bitacoraBajaDTO) throws UserDisplayableException {
+
+        try (
+                Connection connection = DBConnector.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY)
+        ) {
+
+            statement.setInt(1, bitacoraBajaDTO.getIDArticulo());
+            statement.setDate(2, new Date(bitacoraBajaDTO.getFecha().getTime()));
+            statement.setString(3, bitacoraBajaDTO.getMotivo());
+            statement.setInt(4, bitacoraBajaDTO.getCantidadRestante());
+            statement.setInt(5, bitacoraBajaDTO.getIDBitacora());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            throw ExceptionHandler.handleSQLException(
+                    LOGGER,
+                    e,
+                    "No ha sido posible actualizar la bitácora de baja."
+            );
+        }
+    }
+
+    @Override
+    public void deleteOne(Integer id) throws UserDisplayableException {
+
+        try (
+                Connection connection = DBConnector.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement(DELETE_QUERY)
+        ) {
+
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+
+            throw ExceptionHandler.handleSQLException(
+                    LOGGER,
+                    e,
+                    "No ha sido posible eliminar la bitácora de baja."
+            );
+        }
+    }
+}
---
+++ src/main/business/dto/BitacoraBajaDTO.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.business.dto;
+
+import java.util.Date;
+
+/**
+ *
+ * @author josem
+ */
+public class BitacoraBajaDTO {
+
+    private final int idBitacora;
+    private final int idArticulo;
+    private final Date fecha;
+    private final String motivo;
+    private final int cantidadRestante;
+
+    public BitacoraBajaDTO(BitacoraBajaBuilder builder) {
+        this.idBitacora = builder.idBitacora;
+        this.idArticulo = builder.idArticulo;
+        this.fecha = builder.fecha;
+        this.motivo = builder.motivo;
+        this.cantidadRestante = builder.cantidadRestante;
+    }
+
+    public int getIDBitacora() {
+        return idBitacora;
+    }
+
+    public int getIDArticulo() {
+        return idArticulo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public int getCantidadRestante() {
+        return cantidadRestante;
+    }
+
+    @Override
+    public boolean equals(Object instance) {
+        if (this == instance) return true;
+        if (instance == null || getClass() != instance.getClass()) return false;
+
+        BitacoraBajaDTO that = (BitacoraBajaDTO) instance;
+
+        return idBitacora == that.idBitacora
+                && idArticulo == that.idArticulo
+                && cantidadRestante == that.cantidadRestante
+                && fecha.equals(that.fecha)
+                && motivo.equals(that.motivo);
+    }
+
+    public static class BitacoraBajaBuilder {
+
+        protected int idBitacora;
+        protected int idArticulo;
+        protected Date fecha;
+        protected String motivo;
+        protected int cantidadRestante;
+
+        public BitacoraBajaBuilder setIDBitacora(int idBitacora) {
+            this.idBitacora = idBitacora;
+            return this;
+        }
+
+        public BitacoraBajaBuilder setIDArticulo(int idArticulo) {
+            this.idArticulo = idArticulo;
+            return this;
+        }
+
+        public BitacoraBajaBuilder setFecha(Date fecha) {
+            this.fecha = fecha;
+            return this;
+        }
+
+        public BitacoraBajaBuilder setMotivo(String motivo) {
+            this.motivo = motivo;
+            return this;
+        }
+
+        public BitacoraBajaBuilder setCantidadRestante(int cantidadRestante) {
+            this.cantidadRestante = cantidadRestante;
+            return this;
+        }
+
+        public BitacoraBajaDTO build() {
+            return new BitacoraBajaDTO(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BitacoraBaja: " + idBitacora
+                + ", Articulo: " + idArticulo
+                + ", Fecha: " + fecha
+                + ", Motivo: " + motivo
+                + ", Cantidad restante: " + cantidadRestante;
+    }
+}
