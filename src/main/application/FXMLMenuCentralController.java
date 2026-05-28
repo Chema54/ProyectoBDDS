7033b2337df55db4f23cc13847fe8a657706a842
LeninRevan <lenintanooki@gmail.com>
2026-05-28 00:14:01 -0600

    Ya hay ventanas como se indica

--- src/main/application/FXMLMenuCentralController.java
+++ src/main/application/FXMLMenuCentralController.java
@@ -4,14 +4,18 @@
  */
 package main.application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import main.common.Utilidades;
 
@@ -23,8 +27,9 @@ import main.common.Utilidades;
 public class FXMLMenuCentralController implements Initializable {
     
     Utilidades utilidades = new Utilidades();
-    @FXML
     private StackPane stackPane_VistaCentral;
+    @FXML
+    private AnchorPane pane_Escritorio;
 
     /**
      * Initializes the controller class.
@@ -46,7 +51,23 @@ public class FXMLMenuCentralController implements Initializable {
 
     @FXML
     private void irRegistrarUsuarios(ActionEvent event) {
-        utilidades.abrirNuevaPantalla("/main/resources/gui/FXMLRegistroUsuariosView.fxml", stackPane_VistaCentral);
+        try {
+            // 1. Cargas el FXML del formulario que quieres mostrar adentro
+            Node formulario = FXMLLoader.load(getClass().getResource("/main/resources/gui/FXMLRegistroUsuariosView.fxml"));
+
+            // 2. Lo envuelves en tu componente personalizado de ventana flotante
+            VentanaInterna ventana = new VentanaInterna("Registrar Usuario", formulario);
+
+            // 3. Posición inicial en el escritorio para que no se encimen todas arriba a la izquierda
+            ventana.setLayoutX(50);
+            ventana.setLayoutY(50);
+
+            // 4. Lo agregas al AnchorPane/Pane central
+            pane_Escritorio.getChildren().add(ventana);
+
+        } catch (IOException ex) {
+            System.out.println("Error al cargar el formulario: " + ex.getMessage());
+        }
     }
 
     @FXML
